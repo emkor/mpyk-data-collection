e62ca1e9061a7ccd8c86e750334232ac7de2fc2f
emkor93 <emkor93@gmail.com>
2019-05-15 14:30:33 +0200

    added setting up raspbian

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Installing git..."
+sudo apt-get install -y git zip curl
+
+echo "Installing pip for Python 3..."
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+rm get-pip.py
+
+sudo pip install b2
+
+mkdir -p ~/mpk && cd ~/mpk
+git clone https://github.com/emkor/mpyk.git && cd mpyk
+sudo pip install -r requirements.txt && chmod u+x mpyk.py
+
+sudo ln -s ~/mpk/mpyk/mpyk.py /usr/local/bin/mpyk
