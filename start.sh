b7e211a44adabb5aee1fb6d4c6f7a62aec62e56b
emkor93 <emkor93@gmail.com>
2019-05-15 17:01:24 +0200

    I forgot the uppersand; fixed this

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 echo "Starting mpyk in separate process..."
 source ~/mpk/mpyk-data-collection/b2.sh
-nohup mpyk --each 10 --utc --dir ~/mpk/data --log ~/mpk/log/mpyk.log
+nohup mpyk --each 10 --utc --dir ~/mpk/data --log ~/mpk/log/mpyk.log &
 echo "Done!"
