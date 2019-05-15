4d96e2057d74aa24efd1a26412b01f46ee652bf4
emkor93 <emkor93@gmail.com>
2019-05-15 14:54:09 +0200

    updated setup.py script

--- setup.sh
+++ setup.sh
@@ -12,8 +12,8 @@ rm get-pip.py
 
 sudo pip install b2
 
--p ~/mpk/data && cd ~/mpk
-git clone https://github.com/emkor/mpyk.git && cd mpyk
-sudo pip install -r requirements.txt && chmod u+x mpyk.py
+mkdir -p ~/mpk/data && cd ~/mpk
+git clone https://github.com/emkor/mpyk.git && cd ~/mpk/mpyk
+sudo pip install -r requirements.txt && chmod u+x ~/mpk/mpykmpyk.py
 
 sudo ln -s ~/mpk/mpyk/mpyk.py /usr/local/bin/mpyk
