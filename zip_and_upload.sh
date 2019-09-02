6a8b7b1601d616a88cb8ec59407e0377a88e237b
emkor93 <emkor93@gmail.com>
2019-09-02 07:33:12 +0200

    fix bug: uncomment sourcing B2 credentials

--- zip_and_upload.sh
+++ zip_and_upload.sh
@@ -4,7 +4,7 @@ set -e
 CSV_DIR=$1
 
 echo "Running MPK data archival job..."
-#source ~/mpk/mpyk-data-collection/b2.sh
+source ~/mpk/mpyk-data-collection/b2.sh
 b2 authorize-account ${B2_KEY_ID} ${B2_APP_KEY}
 for f in $(find ${CSV_DIR} -name "*.csv" | grep -v $(date --iso-8601 --utc)); do echo "Archiving file $f ..." && zip "$f.zip" ${f} && b2 upload-file --noProgress ${B2_BUCKET} "$f.zip" $(basename "$f.zip") && rm ${f} && rm "$f.zip"; done
 echo "Done archiving stuff"
