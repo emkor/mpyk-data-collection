926ff27191d1e93dc2d1142872d5a36a41c2bccd
emkor93 <emkor93@gmail.com>
2021-01-13 12:26:48 +0100

    there was a case when _flush() to csv file was executed in the middle of archiving that csv file, causing zip file to be corrupted. Reduce executor to single thread to avoid that

--- src/mpyk_collect.py
+++ src/mpyk_collect.py
@@ -46,7 +46,7 @@ class MpykArchiver:
         csv_file_name = path.basename(csv_file_path)
         zip_file_name = path.basename(zip_file_path)
         start_time = time.time()
-        with ZipFile(zip_file_path, 'w', ZIP_DEFLATED, compresslevel=Z_BEST_COMPRESSION) as tmp_zip:
+        with ZipFile(zip_file_path, 'x', ZIP_DEFLATED, compresslevel=Z_BEST_COMPRESSION) as tmp_zip:
             tmp_zip.write(csv_file_path, arcname=csv_file_name)
         took_s = round(time.time() - start_time, ndigits=3)
         log.info(f"Archiving successful in {took_s}s, removing {csv_file_path}")
@@ -71,7 +71,6 @@ class MpykStore:
         self._lock = RLock()
         self._buffer: List[MpykTransLoc] = []
         self._last_chunk_date = datetime.utcnow().date()
-        self.csv_file_lock = RLock()
 
     def add(self, positions: List[MpykTransLoc]):
         new_chunk_date = positions[0].timestamp.date()
@@ -97,10 +96,9 @@ class MpykStore:
     def _flush(self, file_path: str, positions: List[MpykTransLoc]):
         log.debug(f"Flushing buffer with {len(positions)} entries into CSV at {file_path}")
         start_time = time.time()
-        with self.csv_file_lock:
-            with open(file_path, "a+") as out_f:
-                csv_writer = csv.writer(out_f)
-                csv_writer.writerows((trans_loc.as_values() for trans_loc in positions))
+        with open(file_path, "a+") as out_f:
+            csv_writer = csv.writer(out_f)
+            csv_writer.writerows((trans_loc.as_values() for trans_loc in positions))
         took_s = round(time.time() - start_time, ndigits=3)
         log.info(f"Done writing {len(positions)} entries in {took_s}s to CSV at {file_path}")
 
@@ -167,7 +165,7 @@ if __name__ == '__main__':
         log.warning(f"B2_BUCKET_NAME env var is missing, disabling BackBlaze file upload!")
 
     log.info(f"Starting mpyk collecting data each {each_sec}s into daily CSVs at {csv_dir}")
-    executor = ThreadPoolExecutor(max_workers=os.cpu_count())
+    executor = ThreadPoolExecutor(max_workers=1)
     mpyk_client = MpykClient()
     mpyk_archiver = MpykArchiver(executor, tmp_zip_dir,
                                  b2_bucket_factory=lambda: get_b2_bucket(app_key_id, app_key, bucket_name),
